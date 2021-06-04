784d30d86976229043f3a633386030f97a42e7a6
Olejjka413 <Ivanova.Olya2001@yandex.ru>
2021-06-04 12:12:38 +0300

    к

--- theme.js
+++ theme.js
@@ -1,8 +1,3 @@
-<script>
-$(function () 
-{
-  $('.toggle').on('click' ,function(event) {
-    $('body').toggleClass('dark');				
-  })}
-);
-</script>
+toggleTheme.onClick = () => {
+    document.body.classList.toggle("dark")
+}
